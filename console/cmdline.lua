8d6e764f620441310d2431fbb7b65caacdfa33a8
usysrc <tilmann.hars@googlemail.com>
2021-02-11 22:11:00 +0100

    comments

--- console/cmdline.lua
+++ console/cmdline.lua
@@ -1,13 +1,37 @@
+--[[
+Copyright (c) 2021, usysrc
+
+Permission to use, copy, modify, and/or distribute this software for any
+purpose with or without fee is hereby granted, provided that the above
+copyright notice and this permission notice appear in all copies.
+
+THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
+ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
+OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+]]--
+
+--[[
+    File: cmdline.lua
+    Description: a weird tiny little shell
+]]--
+
+--[[
+    Utilities
+]]--
 local bitser = require 'lib.bitser'
 
+--[[
+    Private
+]]--
 local cmdline = {}
-
 local input = ""
 local hbuffer = ""
 local linestart = "> "
-
 local history = {}
-
 
 -- generate a savedir and placeholder file
 love.filesystem.setIdentity("console")
