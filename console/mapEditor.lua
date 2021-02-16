d81719cc6090eff8b7eae5bb67ec5c44a97b7a28
usysrc <tilmann.hars@googlemail.com>
2021-02-16 21:10:45 +0100

    refactored

--- console/mapEditor.lua
+++ console/mapEditor.lua
@@ -34,6 +34,7 @@ local tilesetPicker = require("console.tilesetPicker")
 local saveAndLoad   = require("console.saveAndLoad")
 local saveText      = require("console.saveText")
 local Camera        = require("lib.hump.camera")
+local palette       = require("console.palette")
 
 --[[
     Private
@@ -88,12 +89,18 @@ end
 
 mapEditor.draw = function()
     love.graphics.clear()
+    setColor(255,255,255)
+    love.graphics.rectangle("fill", 0,0, love.graphics.getWidth(), love.graphics.getHeight())
     camera:attach()
-    for i=-64, 64 do
-        for j=-64,64 do
+    for i=-32, 32 do
+        for j=-32,32 do
             local t = map.getData(i, j)
             if t then
+                setColor(255,255,255)
                 love.graphics.draw(sprites.get(t), i*16, j*16)
+            else
+                setColor(palette[1])
+                love.graphics.rectangle("fill", i*16, j*16, 16, 16)
             end
         end
     end
